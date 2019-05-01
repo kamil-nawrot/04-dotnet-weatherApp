c6dc3e5385461e076a5abc799d4bb049e22591ec
Kamil Nawrot <kamilnawrot98@gmail.com>
2019-05-01 22:42:45 +0200

    initial commit (v.1.0.0)

--- WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +23,299 @@ namespace WeatherApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+            Properties.Settings.Default.Reload();
+            LoadWeatherData();
+        }
+
+        public async void LoadWeatherData()
+        {
+            while (true)
+            {
+                HomeCity.Text = Properties.Settings.Default.HomeCity;
+
+                string homeCity = Properties.Settings.Default.HomeCity;
+                string responseXML = await WeatherInfo.LoadInfoAsync(homeCity);
+                WeatherInfo result;
+
+                using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(responseXML)))
+                {
+                    result = WeatherInfo.ParseWeather(stream);
+                    if (Properties.Settings.Default.Temperature == "C") HomeCityTemp.Text = result.KelvinToCelsius().ToString("0.00") + "°C";
+                    else if (Properties.Settings.Default.Temperature == "K") HomeCityTemp.Text = result.Temperature.ToString("0.00") + "K";
+                    else if (Properties.Settings.Default.Temperature == "F") HomeCityTemp.Text = result.KelvinToFahrenheit().ToString("0.00") + "°F";
+                    HomeCityPressure.Text = "PRESSURE: " + result.Pressure.ToString() + "hPa";
+                    HomeCityHumidity.Text = "HUMIDITY: " + result.Humidity.ToString() + "%";
+                    HomeCityWindSpeed.Text = "WIND SPEED: " + result.WindSpeed.ToString() + "km/h";
+                    HomeCityDesc.Text = result.Description;
+                }
+
+                using (var db = new WeatherDbEntities())
+                {
+                    decimal tempTemperature;
+                    if (Properties.Settings.Default.Temperature == "C") tempTemperature = (decimal)result.KelvinToCelsius();
+                    else if (Properties.Settings.Default.Temperature == "K") tempTemperature = (decimal)result.Temperature;
+                    else tempTemperature = (decimal)result.KelvinToFahrenheit();
+
+                    HomeCityInfo newInfo = new HomeCityInfo
+                    {
+                        Id = db.HomeCityInfoes.Count() + 1,
+                        City = homeCity,
+                        Temperature = Decimal.Round(tempTemperature, 2),
+                        TempUnits = Properties.Settings.Default.Temperature,
+                        Description = result.Description,
+                        Pressure = (int)result.Pressure,
+                        Humidity = (int)result.Humidity,
+                        WindSpeed = (decimal)result.WindSpeed,
+                        Time = result.Time.ToString()
+                    };
+
+                    db.HomeCityInfoes.Add(newInfo);
+                    db.SaveChanges();
+                }
+
+                await Task.Delay(300000);
+            }
+        }
+
+        private void HomeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            HomeView.Visibility = Visibility.Visible;
+            SearchView.Visibility = Visibility.Collapsed;
+            SettingsView.Visibility = Visibility.Collapsed;
+        }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            HomeView.Visibility = Visibility.Collapsed;
+            SearchView.Visibility = Visibility.Visible;
+            SettingsView.Visibility = Visibility.Collapsed;
+        }
+
+        private void SettingsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            HomeView.Visibility = Visibility.Collapsed;
+            SearchView.Visibility = Visibility.Collapsed;
+            SettingsView.Visibility = Visibility.Visible;
+
+            HomeCitySetting.Text = Properties.Settings.Default.HomeCity;
+            ChangeHomeCityBtn.IsChecked = false;
+            if (Properties.Settings.Default.Temperature == "C") CelsiusSetting.IsChecked = true;
+            else if (Properties.Settings.Default.Temperature == "K") KelvinSetting.IsChecked = true;
+            else if (Properties.Settings.Default.Temperature == "F") FahrenheitSetting.IsChecked = true;
+        }
+
+        public async void Search ()
+        {
+            searchInProgress = "True";
+            string searchedCity = SearchedCity.Text;
+            string responseXML = await WeatherInfo.LoadInfoAsync(searchedCity);
+            WeatherInfo result;
+
+            using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(responseXML)))
+            {
+                result = WeatherInfo.ParseWeather(stream);
+                if (result != null)
+                {
+                    SearchedCityName.Text = searchedCity;
+                    if (Properties.Settings.Default.Temperature == "C") SearchedCityTemp.Text = result.KelvinToCelsius().ToString("0.00") + "°C";
+                    else if (Properties.Settings.Default.Temperature == "K") SearchedCityTemp.Text = result.Temperature.ToString("0.00") + "K";
+                    else if (Properties.Settings.Default.Temperature == "F") SearchedCityTemp.Text = result.KelvinToFahrenheit().ToString("0.00") + "°F";
+                    SearchedCityPressure.Text = "PRESSURE: " + result.Pressure.ToString() + "hPa";
+                    SearchedCityHumidity.Text = "HUMIDITY: " + result.Humidity.ToString() + "%";
+                    SearchedCityDesc.Text = result.Description;
+                    SearchedCityWindSpeed.Text = "WIND SPEED: " + result.WindSpeed.ToString() + "km/h";
+                }
+            }
+
+            await Task.Delay(1000);
+
+            if (result != null)
+            {
+                SearchResult.Visibility = Visibility.Visible;
+            }
+
+            SearchedCity.Text = String.Empty;
+            searchInProgress = "False";
+        }
+
+        private void StartSearching_Click(object sender, RoutedEventArgs e)
+        {
+            Search();
+        }
+
+        private async void ChangeHomeCityBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string searchedCity = HomeCitySetting.Text;
+            string responseXML = await WeatherInfo.LoadInfoAsync(searchedCity);
+            WeatherInfo result;
+
+            using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(responseXML)))
+            {
+                result = WeatherInfo.ParseWeather(stream);
+                if (result != null)
+                {
+                    Properties.Settings.Default.HomeCity = HomeCitySetting.Text;
+                    Properties.Settings.Default.Save();
+                    LoadWeatherData();
+                }
+            }
+        }
+
+        private async void HomeCitySetting_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key != System.Windows.Input.Key.Enter) return;
+
+            e.Handled = true;
+            string searchedCity = HomeCitySetting.Text;
+            string responseXML = await WeatherInfo.LoadInfoAsync(searchedCity);
+            WeatherInfo result;
+
+            using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(responseXML)))
+            {
+                result = WeatherInfo.ParseWeather(stream);
+                if (result != null)
+                {
+                    ChangeHomeCityBtn.IsChecked = true;
+                    Properties.Settings.Default.HomeCity = HomeCitySetting.Text;
+                    Properties.Settings.Default.Save();
+                    LoadWeatherData();
+                }
+            }
+        }
+
+        private void KelvinSetting_Checked(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.Temperature = "K";
+            Properties.Settings.Default.Save();
+            LoadWeatherData();
+        }
+
+        private void CelsiusSetting_Checked(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.Temperature = "C";
+            Properties.Settings.Default.Save();
+            LoadWeatherData();
+        }
+
+        private void FahrenheitSetting_Checked(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.Temperature = "F";
+            Properties.Settings.Default.Save();
+            LoadWeatherData();
+        }
+
+        private void SearchedCity_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key != System.Windows.Input.Key.Enter) return;
+
+            e.Handled = true;
+            Search();
+        }
+
+        private string _searchInProgress = "False";
+        public string searchInProgress
+        {
+            get { return _searchInProgress; }
+            set
+            {
+                _searchInProgress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName]string searchInProgress = null)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(searchInProgress);
+                handler(this, e);
+            }
+        }
+
+        #endregion
+
+        private void HistoryBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string)HistoryBtn.Content == "SHOW HISTORY")
+            {
+                WeatherImage.Visibility = Visibility.Collapsed;
+                HistoryList.Visibility = Visibility.Visible;
+                AddNewInfoBtn.Visibility = Visibility.Visible;
+                HistoryBtn.Content = "HIDE HISTORY";
+
+                using (var db = new WeatherDbEntities())
+                {
+                    var query = (from info in db.HomeCityInfoes
+                                 select new WeatherInfo
+                                 {
+                                     City = info.City,
+                                     Temperature = (float)info.Temperature,
+                                     TempUnits = info.TempUnits,
+                                     Pressure = info.Pressure,
+                                     Humidity = info.Humidity,
+                                     WindSpeed = (float)info.WindSpeed,
+                                     Description = info.Description,
+                                     Time = info.Time
+                                 });
+
+                    var weatherInfoList = new List<WeatherInfo>(query);
+                    HistoryList.ItemsSource = weatherInfoList;
+                }
+            }
+            else
+            {
+                WeatherImage.Visibility = Visibility.Visible;
+                HistoryList.Visibility = Visibility.Collapsed;
+                AddNewInfoBtn.Visibility = Visibility.Collapsed;
+                HistoryBtn.Content = "SHOW HISTORY";
+            }
+        }
+
+        private void AddNewInfoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (AddNewInfo.Visibility == Visibility.Collapsed)
+            {
+                HistoryList.Visibility = Visibility.Collapsed;
+                AddNewInfo.Visibility = Visibility.Visible;
+                HistoryBtn.IsEnabled = false;
+            }
+            else if (AddNewInfo.Visibility == Visibility.Visible)
+            {
+                using (var db = new WeatherDbEntities())
+                {
+                    HomeCityInfo newInfo = new HomeCityInfo
+                    {
+                        Id = db.HomeCityInfoes.Count() + 1,
+                        City = AddNewCity.Text,
+                        Temperature = decimal.Parse(AddNewTemp.Text),
+                        TempUnits = "C",
+                        Description = AddNewDesc.Text,
+                        Pressure = int.Parse(AddNewPressure.Text),
+                        Humidity = int.Parse(AddNewHumidity.Text),
+                        WindSpeed = decimal.Parse(AddNewWindSpeed.Text),
+                        Time = DateTime.Now.ToString()
+                    };
+
+                    db.HomeCityInfoes.Add(newInfo);
+                    db.SaveChanges();
+                }
+
+                HistoryList.Visibility = Visibility.Visible;
+                AddNewInfo.Visibility = Visibility.Collapsed;
+                HistoryBtn.IsEnabled = true;
+                HistoryBtn_Click(sender, e);
+            }
         }
     }
 }
---
+++ WeatherApp/WeatherInfo.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml;
+
+namespace WeatherApp
+{
+    public class WeatherInfo
+    {
+        static string ApiKey = "0a07712c459a768a3181478df78c4608";
+        static string ApiBaseUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+        public string City { get; set; }
+        public float Temperature { get; set; }
+        public string TempUnits { get; set; }
+        public float Pressure { get; set; }
+        public float Humidity { get; set; }
+        public float WindSpeed { get; set; }
+        public string Description { get; set; }
+        public string Time { get; set; }
+
+        public static async Task<string> LoadInfoAsync(string CityName)
+        {
+            string ApiQuery = ApiBaseUrl + "?q=" + CityName + "&apikey=" + ApiKey + "&mode=xml";
+            Task<string> result;
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(ApiQuery))
+            using (HttpContent content = response.Content)
+            {
+                result = content.ReadAsStringAsync();
+            }
+            return await result;
+        }
+
+        public static WeatherInfo ParseWeather(System.IO.Stream stream)
+        {
+
+            XmlTextReader reader = new XmlTextReader(stream);
+            WeatherInfo result = new WeatherInfo()
+            {
+                City = string.Empty,
+                Temperature = float.NaN,
+                TempUnits = Properties.Settings.Default.Temperature,
+                Pressure = float.NaN,
+                Humidity = float.NaN,
+                WindSpeed = float.NaN,
+                Description = string.Empty,
+                Time = DateTime.Now.ToString()
+            };
+
+            try
+            {
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        switch (reader.Name)
+                        {
+                            case "city":
+                                result.City = reader.GetAttribute("name");
+                                break;
+                            case "temperature":
+                                result.Temperature = float.Parse(reader.GetAttribute("value"),
+                                                                  System.Globalization.CultureInfo.InvariantCulture);
+                                break;
+                            case "pressure":
+                                result.Pressure = float.Parse(reader.GetAttribute("value"));
+                                break;
+                            case "humidity":
+                                result.Humidity = float.Parse(reader.GetAttribute("value"));
+                                break;
+                            case "weather":
+                                result.Description = reader.GetAttribute("value");
+                                break;
+                            case "wind":
+                                reader.ReadToDescendant("speed");
+                                result.WindSpeed = float.Parse(reader.GetAttribute("value"),
+                                                               System.Globalization.CultureInfo.InvariantCulture);
+                                break;
+                            case "message":
+                                result = null;
+                                throw new XmlException();
+                        }
+                    }
+                }
+            }
+            catch (XmlException error)
+            {
+                MessageBox.Show(error.Message);
+                return null;
+            }
+            return result;
+        }
+
+        public float KelvinToCelsius()
+        {
+            return Temperature - (float)273.15;
+        }
+
+        public float KelvinToFahrenheit()
+        {
+            float FTemp = (Temperature - (float)273.15) * (float)1.8 + (float)32;
+            return FTemp;
+        }
+    }
+}
